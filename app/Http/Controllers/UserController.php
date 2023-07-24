1b7ee60a880feb5ba68bc6bd35469e959f1c9ebf
Albub-Yusuf <albubyusuf@gmail.com>
2023-07-24 09:31:17 +0600

    user login with jwt token done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\JWTToken;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
@@ -34,5 +35,24 @@ class UserController extends Controller
 
         }   
 
+    }
+
+    function userLogin(Request $request){
+        $hasUser = User::where('email',$request->input('email'))
+                        ->where('password',$request->input('password'))
+                        ->select('id')->first();
+        
+        if($hasUser !==null){
+            $token = JWTToken::CreateToken($request->input('email'),$hasUser->id);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Login Successful!'
+            ],200)->cookie('token',$token,60*60*24);
+        }else{
+            return response()->json([
+                'status'=>'failed',
+                'message'=>"unauthorized"
+            ],401);
+        }
     }
 }
